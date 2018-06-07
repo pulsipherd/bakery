9e62a857c605854c24b13231e96b555efdbfe486
Deneb Pulsipher <pulsipherd@gmail.com>
2018-06-07 14:25:25 -0600

    validate a few things

---
+++ app/models/ingredient.rb
@@ -0,0 +1,8 @@
+class Ingredient < ApplicationRecord
+  validates_presence_of :name
+  validates_uniqueness_of :name, case_sensitive: false
+
+  has_many :recipe_ingredients, dependent: :destroy
+  has_many :recipes, through: :recipe_ingredients
+
+end
